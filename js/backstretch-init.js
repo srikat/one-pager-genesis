81f78c6da047858a7fd2ecb93e6d9e99dac0c869
Sridhar Katakam <srikat@gmail.com>
2014-01-26 03:07:39 +0530

    updated to fix broken links

--- js/backstretch-init.js
+++ js/backstretch-init.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($) {
 
-	$("#welcome").backstretch("http://websitesetuppro.com/demos/single-page/wp-content/themes/genesis-sample/images/welcome-background.jpg");
+	$("#welcome").backstretch("http://websitesetuppro.com/demos/one-pager-genesis/wp-content/themes/one-pager/images/welcome-background.jpg");
 
-	$("#portfolio").backstretch("http://websitesetuppro.com/demos/single-page/wp-content/themes/genesis-sample/images/portfolio-background.jpg");
+	$("#portfolio").backstretch("http://websitesetuppro.com/demos/one-pager-genesis/wp-content/themes/one-pager/images/portfolio-background.jpg");
 
-	$("#contact").backstretch("http://websitesetuppro.com/demos/single-page/wp-content/themes/genesis-sample/images/contact-background.jpg");
+	$("#contact").backstretch("http://websitesetuppro.com/demos/one-pager-genesis/wp-content/themes/one-pager/images/contact-background.jpg");
 
-	$("#about").backstretch("http://websitesetuppro.com/demos/single-page/wp-content/themes/genesis-sample/images/about-background.jpg");
+	$("#about").backstretch("http://websitesetuppro.com/demos/one-pager-genesis/wp-content/themes/one-pager/images/about-background.jpg");
 
 });
